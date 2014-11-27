a959e0658e5ffb71222bcc04cbb34f1658522eec
xuehuibo <xuehuibo@qq.com>
2014-11-27 22:08:04 +0800

    创建基本目录

---
+++ Project/CRM/CRM/Scripts/main.js
@@ -0,0 +1,25 @@
+﻿require.config({
+    baseUrl:'/Scripts/',
+    paths: {
+/*        'jquery': ['jquery'],
+        'backbone': ['backbone'],
+        'underscore': ['underscore'],
+        'bootstrap': ['bootstrap']*/
+    },
+    shim: {
+        'jquery': {
+          exports:'$'  
+        },
+        'underscore': {
+            exports:'_'
+        },
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports:'Backbone'
+        },
+        'bootstrap': {
+            deps: ['jquery', 'css!/Content/css/bootstrap.min.css', 'css!../Content/css/dashboard.css']
+        }
+    }
+});
+require(['jquery', 'underscore', 'bootstrap', 'backbone']);
